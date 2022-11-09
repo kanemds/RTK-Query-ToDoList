811ce333d2ff24e1826d5145224218720c842cd9
Kanem <kanemdeng@email.com>
2022-11-09 15:29:38 -0800

    able to delete user

--- server/api/all/user.js
+++ server/api/all/user.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
+const deleteUser = require('../../controllers/deleteControoler')
 const { register } = require('../../controllers/registerController')
 const userUpdate = require('../../controllers/updateController')
 
 router.post('/register', register)
 router.put('/update/:id', userUpdate)
+router.delete('/delete/:id', deleteUser)
 
 module.exports = router
 
---
+++ server/controllers/deleteControoler.js
@@ -0,0 +1,13 @@
+const User = require('../models/user')
+
+const deleteUser = async (req, res) => {
+  try {
+    const id = req.params.id
+    const deletedUser = await User.findByIdAndDelete(id)
+    res.status(200).json("User has been delete")
+  } catch (error) {
+    res.status(401).json("Not authenticated")
+  }
+}
+
+module.exports = deleteUser
