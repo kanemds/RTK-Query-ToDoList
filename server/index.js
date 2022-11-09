4e118b5c6ffc9c16ef05e0493b3ee5b94a1235e7
Kanem <kanemdeng@email.com>
2022-11-09 14:10:33 -0800

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const PORT = 3001
+const DB = process.env.MONGODB_URL
 
-app.listen(PORT, () => {
-  console.log(`connted to server: ${PORT}`)
-})
+mongoose.connect(DB)
+  .then(() => {
+    console.log('connected to DB')
+    app.listen(PORT, () => {
+      console.log(`connted to server: ${PORT}`)
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+
 
 app.get('/', (req, res) => {
   res.send('hey')
