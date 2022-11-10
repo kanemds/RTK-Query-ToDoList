5918cd3aa6e97fad3428baad7492eb9e54cd95dd
Kanem <kanemdeng@email.com>
2022-11-09 22:12:23 -0800

    adding bcrypt for passwrod

--- server/controllers/registerController.js
+++ server/controllers/registerController.js
@@ -1,10 +1,14 @@
 
+const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
+
 const register = async (req, res) => {
-  console.log(req.body)
+
   try {
-    const newUser = new User(req.body)
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(req.body.password, salt)
+    const newUser = new User({ ...req.body, password: hash })
 
     await newUser.save()
     res.status(200).json("New User has been created.")
