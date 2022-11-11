b03c7ee00d74b1053a99d552b65b1cd5a76105a3
Kanem <kanemdeng@email.com>
2022-11-10 17:59:26 -0800

    created an api slice

---
+++ client/src/features/api/apiSlice.js
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  endpoints: (builder) => ({
+    getTodos: builder.query({
+      query: () => '/todo-list'
+    })
+  })
+})
+
+export const { useGetTodosQuery } = apiSlice
