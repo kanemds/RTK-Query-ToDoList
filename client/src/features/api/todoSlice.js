85fbca9322f6a0edbe19a4e2bd1d4227ed78ab3f
Kanem <kanemdeng@email.com>
2022-11-11 02:03:55 -0800

    adding create and update todo query

--- client/src/features/api/todoSlice.js
+++ client/src/features/api/todoSlice.js
@@ -4,6 +4,20 @@ export const todoApiSlice = apiSlice.injectEndpoints({
   endpoints: builder => ({
     getTodos: builder.query({
       query: () => '/todo-list'
+    }),
+    addTodo: builder.mutation({
+      query: todo => ({
+        url: '/todo-list',
+        method: 'POST',
+        body: todo
+      })
+    }),
+    updateTodo: builder.mutation({
+      query: todo => ({
+        url: `/todo-list/${todo._id}`,
+        method: 'PUT',
+        body: todo
+      })
     })
   })
 })
