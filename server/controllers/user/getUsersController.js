b3f5dbb25e8970b003d50f2cfe78b311dfc3c3f9
Kanem <kanemdeng@email.com>
2022-11-12 04:35:05 -0800

    getUserSlice and able to print out all users

--- server/controllers/user/getUsersController.js
+++ server/controllers/user/getUsersController.js
@@ -2,7 +2,10 @@ const User = require('../../models/user')
 
 const getUsers = async (req, res) => {
   try {
-    const users = await User.find()
+    // only one
+    // const users = await User.find().select("-password")
+    const users = await User.find({}, { name: 1 }) // 1 selected field, {name:0} deselecting field
+
     res.status(200).json(users)
   } catch (error) {
     res.status(401).json("Not Authenticated")
