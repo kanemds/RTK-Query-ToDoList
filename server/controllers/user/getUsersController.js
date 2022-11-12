28692ba3d975f94f4d8c9b3213e843eb2e8f13e1
Kanem <kanemdeng@email.com>
2022-11-12 03:28:19 -0800

    able to see users

---
+++ server/controllers/user/getUsersController.js
@@ -0,0 +1,12 @@
+const User = require('../../models/user')
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find()
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(401).json("Not Authenticated")
+  }
+}
+
+module.exports = getUsers
