cb80af330137963cffd559bc728d484733a369c7
Kanem <kanemdeng@email.com>
2022-11-13 00:41:59 -0800

    adding update and delete on userSlice

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -4,18 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: '/' }),
-  tagTypes: ['Todos'],
+  tagTypes: ['Todos', 'Users'],
   endpoints: builder => ({})
 })
 
-// export const apiSlice = createApi({
-//   reducerPath: 'api',
-//   baseQuery: fetchBaseQuery({ baseUrl: '/' }),
-//   endpoints: (builder) => ({
-//     getTodos: builder.query({
-//       query: () => '/todo-list'
-//     })
-//   })
-// })
-
-// export const { useGetTodosQuery } = apiSlice
