aa52bd33dd1cdaf772f0f1724b66522e4644f942
Kanem <kanemdeng@email.com>
2022-11-16 03:53:39 -0800

    added persist prevent user data lost when refresh page

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,15 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import { apiSlice } from "./api/apiSlice"
 import authReducer from "./reducers/authSlice"
+import {
+  FLUSH, PAUSE,
+  PERSIST, persistReducer, persistStore, PURGE,
+  REGISTER, REHYDRATE
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage
+}
+
+const rootReducer = combineReducers({
+  [apiSlice.reducerPath]: apiSlice.reducer,
+  auth: authReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authReducer
-  },
+  reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    }).concat(apiSlice.middleware),
   // set false when deploy
   devTools: true
 })
 
+export const persistor = persistStore(store)
